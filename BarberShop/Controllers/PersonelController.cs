a3df57513f4178aded7447bf744f39ca7e9ff2bc
Mustafa Sait Karadağ <sait.karadag78@gmail.com>
2024-12-12 22:01:00 +0300

    Personel Model ve Controller

--- BarberShop/Controllers/PersonelController.cs
+++ BarberShop/Controllers/PersonelController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using BarberShop.Models;
 
 namespace BarberShop.Controllers
 {
     public class PersonelController : Controller
     {
-        public IActionResult Index()
+        static List<Personel> personeller = new List<Personel>();
+        public IActionResult PersonelEkle()
         {
             return View();
+        }
+        public IActionResult PerKaydet(Personel per)
+        {
+            if (ModelState.IsValid)
+            {
+                //Kayıt İşlemi
+                personeller.Add(per);
+                TempData["msj"] = per.PersonelAd + " adlı personel Kaydedildi";
+                return RedirectToAction("PersonelEkle");
+
+            }
+            TempData["msj"] = "Kayıt İşlemi Başarısız";
+            return RedirectToAction("PersonelEkle");
+            //Hata işlemi
+        }
+
+        public IActionResult PersonelList()
+        {
+            return View(personeller);
         }
     }
 }
---
+++ BarberShop/Models/Personel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberShop.Models
+{
+    public class Personel
+    {
+
+
+        [Required(ErrorMessage = "Bu Alan Boş Bırakılamaz")]
+        [Display(Name = "Personel ID")]
+        public int PersonelID { get; set; }
+
+
+        [Required(ErrorMessage = "Bu Alan Boş Bırakılamaz")]
+        [Display(Name = "Personel Ad")]
+        public string PersonelAd { get; set; }
+
+
+        [Required(ErrorMessage = "Bu Alan Boş Bırakılamaz")]
+        [Display(Name = "Personel Soyad")]
+        public string PersonelSoyad { get; set; }
+
+
+        public List<HizmetUcret> Hizmetler { get; set; }
+    }
+
+    public class HizmetUcret
+    {
+        [Required(ErrorMessage = "Bu Alan Boş Bırakılamaz")]
+        [Display(Name = "Hizmet Adi")]
+        public string Ad { get; set; }
+
+        [Required(ErrorMessage = "Bu Alan Boş Bırakılamaz")]
+        [Display(Name = "Hizmet Ucreti")]
+        public int Ucret { get; set; }
+    }
+}
