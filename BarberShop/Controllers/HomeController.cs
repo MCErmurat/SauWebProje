bf27635029a6d5eb50040384e76640ecdf0db763
Mustafa Sait Karadağ <sait.karadag78@gmail.com>
2024-12-13 22:29:10 +0300

    Hakkımızda Sayfasi Eklendi

--- BarberShop/Controllers/HomeController.cs
+++ BarberShop/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace BarberShop.Controllers
             return View();
         }
 
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
