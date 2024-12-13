94cbf1af20a25eb0f220ed5468b5ec0e5e578c1b
Mehmet Can Ermurat <ermuratmc@gmail.com>
2024-12-13 15:18:51 +0300

    RandevuController ve form elemanları eklendi

---
+++ BarberShop/Controllers/AppointmentsController.cs
@@ -0,0 +1,35 @@
+﻿using BarberShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarberShop.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        // Randevu formunu göster
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // Randevu formunu gönder
+        [HttpPost]
+        public IActionResult Create(Appointments appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                // Burada randevuyu veri tabanına kaydedebilirsiniz.
+                // Örnek: _context.Appointments.Add(appointment);
+                // _context.SaveChanges();
+
+                return RedirectToAction("Success");
+            }
+            return View(appointment);
+        }
+
+        // Başarı sayfası
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ BarberShop/Models/Appointments.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberShop.Models
+{
+    public class Appointments
+    {
+        [Required]
+        [Display(Name = "Ad Soyad")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Telefon Numarası")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Randevu Tarihi")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "Randevu Saati")]
+        public TimeSpan Time { get; set; }
+    }
+}
